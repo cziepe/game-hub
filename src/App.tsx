a5e258e9af1e0d197ee569e3783f44920d9befe2
Colin Ziepe <cziepe@gmail.com>
2024-09-30 21:24:19 +0100

    Highlight selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ const App = () => {
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList
+            selectedGenre={selectedGenre}
             onSelectGenre={(genre) => setSelectedGenre(genre)}
           ></GenreList>
         </GridItem>
