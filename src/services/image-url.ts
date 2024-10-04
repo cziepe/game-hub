c20c3c932a30c32f567f16e532fe1c050489cb26
Colin Ziepe <cziepe@gmail.com>
2024-10-04 16:04:03 +0100

    Handle games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
 
   const media = "media/";
   const index = url.indexOf(media) + media.length;
